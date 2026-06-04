eceaf77c916686ae87553bc7e58506cc86f13fe6
Abdullah Hanif Arif <96676918+abdullahhanifarif@users.noreply.github.com>
2026-06-04 22:14:49 +0700

    + Description

--- SupervisedLearning/src/preprocessor.py
+++ SupervisedLearning/src/preprocessor.py
@@ -7,17 +7,28 @@ class Preprocessor:
         self.df = df
 
     def preprocess(self, y,x):
+        # Selecting the target variable (Y)
         Y = self.df[y]
+
+        # Selecting the feature variables (X)
         X = self.df[x]
+
+        # Splitting the data into training and testing sets (80:20)
         return train_test_split(
                 X, Y,
                 test_size=0.2,
                 random_state=42
             )
-    
+
+
     def preprocess_with_dummies(self, y, x, dummy_columns):
+        # Selecting the target variable (Y)
         Y = self.df[y]
+
+        # Selecting the feature variables (X)
         X = self.df[x].copy()
+
+        # Converting specified categorical columns into dummy/one-hot encoded variables
         X = pd.get_dummies(
             X,
             columns=dummy_columns,
@@ -25,6 +36,7 @@ class Preprocessor:
             dtype=int
         )
 
+        # Splitting the data into training and testing sets (80:20)
         return train_test_split(
             X,
             Y,
