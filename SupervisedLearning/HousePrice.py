78ba50e9d9923c7860ffe66af66f43ec50adf4e6
Abdullah Hanif Arif <96676918+abdullahhanifarif@users.noreply.github.com>
2026-06-06 00:19:16 +0700

    Initial EDA and  Data Cleaning

--- SupervisedLearning/HousePrice.py
+++ SupervisedLearning/HousePrice.py
@@ -12,5 +12,38 @@ def main():
     print("Rows:", df.shape[0])
     print("Columns:", df.shape[1])
 
+
+    #======================= Initial EDA =======================
+    # View the first few rows (default is 5 rows)
+    print(df.head())
+
+    # Check structure, data types, missing values
+    print(df.info())
+
+    # Summary statistics
+    print(df.describe())
+
+    
+
+    #===================== DATA CLEANING ======================
+    #Check missing values
+    print("\n","Missing values:")
+    print(df.isna().sum())
+    print()
+
+    # Check data price
+    print("Rows with price <= 0:", (df["price"] <= 0).sum())
+
+    #Count duplicates
+    print('Number of duplicate data :', df.duplicated().sum())
+
+    # # Remove duplicates
+    # df = df.drop_duplicates()
+
+
+
+    #============== Further EDA & Visualization ==============
+    
+
 if __name__ == "__main__":
     main()
