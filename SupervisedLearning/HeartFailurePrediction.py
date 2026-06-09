cae171509683c8301a9175c06e351310ec88c158
Abdullah Hanif Arif <96676918+abdullahhanifarif@users.noreply.github.com>
2026-06-09 21:37:03 +0700

    + Initial EDA & Data Cleaning

--- SupervisedLearning/HeartFailurePrediction.py
+++ SupervisedLearning/HeartFailurePrediction.py
@@ -7,10 +7,41 @@ def main():
     loader = DataLoader(path)
 
     df = loader.load_data()
-
     print("Loaded dataset from:", path)
+
+    #============================================= Initial EDA =============================================
     print("Rows:", df.shape[0])
     print("Columns:", df.shape[1])
+
+    #View the first few rows (default is 5 rows)
+    print(df.head())
+
+    #Check structure, data types, missing values
+    print(df.info())
+
+    #Summary statistics
+    print(df.describe())
+
+
+    #=========================================== DATA CLEANING ============================================
+    #Check missing values
+    print("\n","Missing values:")
+    print(df.isna().sum())
+    print()
+
+    #Check data price
+    print("Rows with price <= 0:", (df["price"] <= 0).sum())
+
+    #Count duplicates
+    print('Number of duplicate data :', df.duplicated().sum())
+
+    # #Remove duplicates
+    # df = df.drop_duplicates()
+
+
+    
+
+
 
 if __name__ == "__main__":
     main()
